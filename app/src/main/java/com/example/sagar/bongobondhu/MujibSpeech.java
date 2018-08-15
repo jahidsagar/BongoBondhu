d918102284970f2ec92c1b5c593221aaea78e19b
sagar <jahidsagar@gmail.com>
2018-08-15 09:35:14 +0600

    itemclicklistener not working , no music is added

--- app/src/main/java/com/example/sagar/bongobondhu/MujibSpeech.java
+++ app/src/main/java/com/example/sagar/bongobondhu/MujibSpeech.java
@@ -1,18 +1,20 @@
 package com.example.sagar.bongobondhu;
 
+//no need this class , this will be used for different purpose
+
 public class MujibSpeech {
-    String[] mujib_speech , speech_description;
-
-    MujibSpeech(String[] mujib_speech ,String[] speech_description){
-        this.mujib_speech = mujib_speech;
-        this.speech_description = speech_description;
-    }
-
-    public String[] getMujib_speech() {
-        return mujib_speech;
-    }
-
-    public String[] getSpeech_description() {
-        return speech_description;
-    }
+//    String[] mujib_speech , speech_description;
+//
+//    MujibSpeech(String[] mujib_speech ,String[] speech_description){
+//        this.mujib_speech = mujib_speech;
+//        this.speech_description = speech_description;
+//    }
+//
+//    public String[] getMujib_speech() {
+//        return mujib_speech;
+//    }
+//
+//    public String[] getSpeech_description() {
+//        return speech_description;
+//    }
 }
